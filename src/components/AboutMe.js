1ec6b7b0bb37d22804596dee8dcd8cd80c4a6db9
Michael Albright <msalbrig@gmail.com>
2024-10-31 14:15:28 -0400

    finalize AboutMet

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -16,15 +16,33 @@ function AboutMe() {
                 to giving 100% to all that I do. 
                 </p>
                 <p className="about-content mt-4">
-                My development skills started in Java Web Development and have branched out into 
-                many aspects of software engineering. I am a Certified AWS Associate Developer with 
-                2 years of hands-on work experience 
-                practical experience in serverless architectures, event-driven ??? 
-                I have a solid understanding of CICD development
-                in an Agile environment.
+                As an accomplished Software Engineer with strong knowledge of AWS cloud infrastructure, 
+                microservices, and end-to-end development, I bring a unique blend of skills in system 
+                architecture, deployment automation, and application resiliency. Starting as a Java Web 
+                API Developer and advancing through 3 years of hands-on, practical experience, I’ve cultivated a 
+                deep understanding of the full software lifecycle and a rich AWS toolkit. Currently, I manage, 
+                troubleshoot, and innovate on 2 highly dynamic ECS-based REST services with modular designs, 
+                serving live production workloads via an Angular UI on S3 and supported by RDS Postgres, Oracle, 
+                and DynamoDB configurations.
                 </p>
                 <p className="about-content mt-4">
-                With experience gained from many years in the hospitality industry, I bring a great
+                I collaborated heavily on transforming a monolithic legacy service into agile, 
+                microservices, including a batch-processing system orchestrated with 
+                an event-driven architecture utilizing EventBridge, StepFunctions, and Lambdas 
+                and a completetly Configuration-Driven REST Service built on DynamoDb 
+                for accessing secure data in Oracle RDS based on user's roles and input.
+                Leveraging my AWS Certified Developer Associate credential, I’ve implemented 
+                sophisticated health checks, data transformation routines, and parameterized 
+                configurations throughout our services. I’m also well versed in Docker, Jenkins, 
+                Nexus, and the automation of complex deployment workflows, managing everything 
+                from database scripts to CI/CD pipeline integrations. My work ensures seamless, 
+                secure deployments across Parameter Store, Secrets Manager, Route53, and CloudWatch 
+                and a commitment to stability, innovation, and cloud-native efficiency.
+                </p>
+                <p className="about-content mt-4">
+                While my technical skills are current, polished, and growing daily I lean on my 
+                personality traits to be the best employee and colleague I can be. With the 
+                experience gained from many years in the hospitality industry, I bring a great
                 attitude and positivity to all aspects of my life. I pride myself on my abilities to 
                 bring people together by loosening up the insecurities of those around me and forming 
                 a teamwork mentality among all walks of life.
